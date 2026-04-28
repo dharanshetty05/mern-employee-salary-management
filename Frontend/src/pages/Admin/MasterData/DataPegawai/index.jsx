2f490284cb29d0e2bd05f9297cf874011c0a9e8d
dharanshetty05 <dharanshetty05@gmail.com>
2026-04-28 11:32:13 +0530

    fix(LF-105): Enhanced responsive employee table scrolling on mobile

--- Frontend/src/pages/Admin/MasterData/DataPegawai/index.jsx
+++ Frontend/src/pages/Admin/MasterData/DataPegawai/index.jsx
@@ -188,7 +188,7 @@ const DataPegawai = () => {
                 </div>
 
                 <div className="max-w-full overflow-x-auto py-4">
-                    <table className="w-full table-auto">
+                    <table className="min-w-[1100px] table-auto whitespace-nowrap">
                         <thead>
                             <tr className="bg-gray-2 text-left dark:bg-meta-4">
                                 <th className="py-4 px-4 font-medium text-black dark:text-white xl:pl-11">No</th>
